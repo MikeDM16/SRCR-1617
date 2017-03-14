1043e9a0133704bfd5c862be1ae1faad2fe75c81
Rui Filipe Castro Leite <ruicastro@outlook.com>
2017-03-14 22:03:34 +0000

    Remocao e adição

--- Exercício 1/Resolução.pl
+++ Exercício 1/Resolução.pl
@@ -21,9 +21,84 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado cliente: IdUt, Nome, Idade, Morada -> {V,F,D}
 
-% Invariante Estrutural:  nao permitir a insercao de conhecimento
-%                         repetido
+cliente(1,diogo,20,braga).
 
-+cliente(Id, N, I, M) :: (solucoes((P, F), (pai(P, F)), S),
-               comprimento(S, N), 
-			   N == 1).
+% Invariante Estrutural: nao permitir a insercao de conhecimento
+%                        repetido
+
++cliente(ID,N,I,M) :: (solucoes(ID, (cliente(ID, N, I, M)), S),
+                       comprimento(S, L), 
+                       L == 1).
+
+% Invariante Estrutural: garantir a remoção de conhecimento
+
+-cliente(ID,N,I,M) :: (solucoes(ID, (cliente(ID, N, I, M)), S),
+                       comprimento(S, L), 
+                       L == 0).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado cuidado prestado: 
+% IdServ, Descrição, Instituição, Cidade -> {V,F,D}
+
+% Invariante Estrutural: nao permitir a insercao de conhecimento
+%                        repetido
+
++cuidado(ID,D,I,C) :: (solucoes(ID, (cuidado(ID, D, I, C)), S),
+                       comprimento(S, L), 
+                       L == 1).
+
+% Invariante Estrutural: garantir a remoção de conhecimento
+
+-cuidado(ID,D,I,C) :: (solucoes(ID, (cuidado(ID, D, I, C)), S),
+                       comprimento(S, L), 
+                       L == 0).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado ato medico: 
+% Data, IdUtente, IdServico, Custo -> {V,F,D}
+
+% Invariante Estrutural: nao permitir a insercao de conhecimento
+%                        repetido
+
++ato(D,IDU,IDS,C) :: (solucoes(ID, (cuidado(ID, D, I, C)), S),
+                      comprimento(S, L), 
+                      L == 1).
+
+% Invariante Estrutural: garantir a remoção de conhecimento
+
+-ato(D,IDU,IDS,C) :: (solucoes(ID, (cuidado(ID, D, I, C)), S),
+                      comprimento(S, L), 
+                      L == 0).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado solucoes
+
+solucoes(X,Y,Z) :- findall(X,Y,Z).
+
+%---------------------------------- - - - - - - - - - -  -  -  -  -  -
+% Predicado «comprimento» que calcula o número de elementos
+% existentes numa lista
+
+comprimento(L,S) :- length(L,S). 
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que permite a evolucao do conhecimento
+
+evolucao( Termo ) :- (solucoes(INV, +Termo::INV, LINV),
+                      assert(Termo),
+                      testa(LINV)).
+evolucao( Termo ) :- retract(Termo).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que permite a redução do conhecimento
+
+reducao( Termo ) :- (solucoes(INV, -Termo::INV, LINV),
+                     retract(Termo),
+                     testa(LINV)).
+reducao( Termo ) :- assert(Termo).
+
+%--------------------------------------------------------------------
+% Testa
+
+testa([]).
+testa([H|T]) :- H,testa(T).
