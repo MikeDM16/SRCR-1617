40ef637921aadc93f0cd34f9a47da479a9d93871
Rui Filipe Castro Leite <ruicastro@outlook.com>
2017-05-12 23:45:52 +0100

    resultados do miguel e do rui

--- Exercício 3/Procura.R
+++ Exercício 3/Procura.R
@@ -3,6 +3,9 @@ library(leaps)
 ------------------------------------------ FatigueLevel 7 niveis -------------------------------------------
 dados <- read.csv("~/OneDrive/Documentos/MiEI/3.º ano/2.º semestre/Sistemas de Representação de Conhecimento e Raciocínio/Trabalhos práticos/SRCR-1617/Exercício 3/exaustao_FatigueLevel_7.csv", header= TRUE, sep = ';', dec = ',' )
 
+------------------------------------------ FatigueLevel 2 niveis -------------------------------------------
+dados <- read.csv("~/OneDrive/Documentos/MiEI/3.º ano/2.º semestre/Sistemas de Representação de Conhecimento e Raciocínio/Trabalhos práticos/SRCR-1617/Exercício 3/exaustao_FatigueLevel_2.csv", header= TRUE, sep = ';', dec = ',' )
+  
 
 reggi <- regsubsets(FatigueLevel ~ Performance.KDTMean + Performance.MAMean + Performance.MVMean+ Performance.TBCMean + Performance.DDCMean +Performance.DMSMean + Performance.AEDMean + Performance.ADMSLMean + Performance.Task,dados, method = "backward")
 summary(reggi)
